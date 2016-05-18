3d56398c55c06c669610e42a58b0a589b3411b4b
Bogdan Sima <simadragosbogdan@gmail.com>
2016-05-18 21:18:34 +0200

    Only keep input (to pnm) image and output SVGs

--- engine/doesitwork.py
+++ engine/doesitwork.py
@@ -8,6 +8,7 @@ from SimpleCV import Image
 PATH_TO_PROJECT = os.path.expanduser('~/vectoring')
 INPUT_DIR = os.sep.join([PATH_TO_PROJECT, 'engine/in'])
 OUTPUT_DIR = os.sep.join([PATH_TO_PROJECT, 'engine/out'])
+TMP_DIR = '/tmp'
 ALLOWED_IMAGE_FILE_FORMATS = ['png', 'jpeg']
 
 
@@ -30,25 +31,29 @@ def edgify(in_full_path):
     return out_full_path
 
 
-def copy(in_full_path):
+def copy(in_full_path, out_dir):
     (root, ext) = os.path.splitext(in_full_path)
     new_file_name = get_random_string() + ext
-    out_full_path = os.sep.join([OUTPUT_DIR, new_file_name])
-    os.system("cp %s %s" % (in_full_path, out_full_path))
+    out_full_path = os.sep.join([TMP_DIR, new_file_name])
+    os.system("cp %s %s" % (in_full_path, out_dir))
     return out_full_path
 
 
-def clear_output_dir():
-    os.system('rm -rf %s/*.* ' % OUTPUT_DIR)
+def clear_dir(dir_name):
+    if dir_name and dir_name.strip != '/':
+        os.system('rm -rf %s/*.* ' % dir_name)
+    else:
+        print 'this is dangerous my friend.'
 
 
 def remove_intermediates():
-    os.system('find %s -name "*.pnm" -type f -delete' % OUTPUT_DIR)
+    os.system('find %s -name "*.pnm" -type f -delete' % TMP_DIR)
 
 
-def to_pnm(in_full_path):
+def to_pnm(in_full_path, out_full_path=None):
     (root, ext) = os.path.splitext(in_full_path)
-    out_full_path = root + '_pnm.pnm'
+    if not out_full_path:
+        out_full_path = root + '_pnm.pnm'
     os.system("convert %s %s" % (in_full_path, out_full_path))
     return out_full_path
 
@@ -57,6 +62,8 @@ def to_svg(in_full_path):
     (root, ext) = os.path.splitext(in_full_path)
     out_full_path = root + '_svg.svg'
     os.system("potrace -s -o %s %s" % (out_full_path, in_full_path))
+
+    copy(out_full_path, OUTPUT_DIR)
     return out_full_path
 
 
@@ -72,17 +79,15 @@ def get_files(path):
     return list_of_files
 
 
-def compute_1(cpy_full_path):
+def compute_1(pnm_full_path):
     # .{png,jpeg} -> pnm -> svg -> edgified
-    pnm_full_path = to_pnm(cpy_full_path)
     edg_full_path = edgify(pnm_full_path)
     svg_full_path = to_svg(edg_full_path)
     return svg_full_path
     
 
-def compute_2(cpy_full_path):
+def compute_2(pnm_full_path):
     # .{png,jpeg} -> pnm -> svg -> pnm -> edgified -> svg
-    pnm_full_path = to_pnm(cpy_full_path)
     svg_full_path = to_svg(pnm_full_path)
     pnm_full_path = to_pnm(svg_full_path)
     edg_full_path = edgify(pnm_full_path)
@@ -91,16 +96,21 @@ def compute_2(cpy_full_path):
 
 
 def send_files_to_processor():
-    # remove already generated assets
-    clear_output_dir()
-
+    # remove old assets
+    clear_dir(TMP_DIR)
+    clear_dir(OUTPUT_DIR)
     # generate assets
     list_of_images = get_files(INPUT_DIR)
     for in_full_path in list_of_images:
 
-        cpy_full_path = copy(in_full_path)
-        compute_1(cpy_full_path)
-        compute_2(cpy_full_path)
+        (root, ext) = os.path.splitext(in_full_path)
+        new_file_name = get_random_string() + '_pnm.pnm'
+        out_full_path = os.sep.join([TMP_DIR, new_file_name])
+
+        pnm_full_path = to_pnm(in_full_path, out_full_path)
+        copy(pnm_full_path, OUTPUT_DIR)
+        compute_1(pnm_full_path)
+        compute_2(pnm_full_path)
 
         #remove_intermediates()
 
